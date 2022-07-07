1ca6806aa5c83102d10ba7081b4b412c13f5f6fd
zs <2283683403@qq.com>
2022-07-07 17:01:04 +0800

    feat(语法检测模板): 语法检测模板

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    1
-  </div>
+  <div>123</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup></script>
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style lang="scss" scoped></style>
